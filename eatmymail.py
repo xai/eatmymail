fd7ed238b1195bf2d9ad09783436348acda8d575
Olaf Lessenich <xai@linux.com>
2015-07-06 19:34:59 +0200

    Minor formatting.
    Signed-off-by: Olaf Lessenich <xai@linux.com>

--- eatmymail.py
+++ eatmymail.py
@@ -15,6 +15,7 @@
 # License along with this program; if not, write to the
 # Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 # Boston, MA 021110-1307, USA.
+
 import argparse
 
 import mailbox
